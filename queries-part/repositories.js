28db65824da359da56f6cfeb8319998223236993
Patryk Wlazłowicz <patrykwlazlowiczHTD@htdevelopers.com>
2019-01-13 19:36:24 +0100

    Separate getting user repo and contributed repo

--- queries-part/repositories.js
+++ queries-part/repositories.js
@@ -1,4 +1,7 @@
-module.exports = `repositoriesContributedTo(first: 100) {
+module.exports = `repositories(first: 100,
+                               privacy: PRIVATE,
+                               affiliations: [ORGANIZATION_MEMBER, COLLABORATOR, OWNER],
+                               ownerAffiliations: [ORGANIZATION_MEMBER, COLLABORATOR, OWNER]) {
                     edges {
                         node {
                             name
